406d44590dbb228f90f45b5036f6c70a2da03d71
seitosugino <0jb4f61k3853v2f@gmail.com>
2021-09-12 12:10:35 +0000

    トップ画像追加

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,9 @@ Rails.application.routes.draw do
   root :to => "items#top"
   get "items/about" => "items#about"
 
-  
+  resources :dmins
+  resources :customers
   resources :items
+  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
