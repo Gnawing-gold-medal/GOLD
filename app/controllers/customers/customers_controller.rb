e9f2c1add702e248b9a356a247dc58c64bdf96b2
304089 <9457imai2@gmail.com>
2021-09-14 06:37:07 +0000

    マイページ作成

--- app/controllers/customers/customers_controller.rb
+++ app/controllers/customers/customers_controller.rb
@@ -1,2 +1,17 @@
 class Customers::CustomersController < ApplicationController
+
+  def show
+    @customer = Customer.find(params[:id])
+  end
+
+  def leave_page
+    @customer = Customer.find(params[:id])
+  end
+
+  def leave
+    customer = Customer.find(params[:id])
+    customer.update(is_deleted: true)
+    redirect_to root_path
+  end
+
 end
--- app/models/customer.rb
+++ app/models/customer.rb
@@ -3,8 +3,9 @@ class Customer < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-         
+
   has_many :addresses, dependent: :destroy
   has_many :cartitems, dependent: :destroy
   has_many :orders, dependent: :destroy
+
 end
