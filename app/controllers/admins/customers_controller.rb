63ed9968b5928137a7858e51080a2e1bc32e063f
seitosugino <0jb4f61k3853v2f@gmail.com>
2021-09-14 12:49:27 +0000

    ユーザー詳細

--- app/controllers/admins/customers_controller.rb
+++ app/controllers/admins/customers_controller.rb
@@ -2,8 +2,20 @@ class Admins::CustomersController < ApplicationController
   def index
     @customers = Customer.all
   end
+  
   def show
     @customer = Customer.find(params[:id])
+    if @customer.user_status == false
+      @status = "有効"
+    else
+      v = "無効"
+    end
+ 
   end
+  
+  private
+    def customer_params
+     params.require(:customer).permit(:last_name, :first_name, :last_name_kana, :first_name_kana, :postal_code, :address, :telephone_number, :is_deleted)
+    end
   
 end
