316c5b9a0e6dd9e2e15560ce54ab1ea17be218a3
304089 <9457imai2@gmail.com>
2021-09-15 12:24:09 +0000

    注文履歴一覧作成

--- app/models/order.rb
+++ app/models/order.rb
@@ -2,4 +2,5 @@ class Order < ApplicationRecord
   belongs_to :customer
   has_many :order_items, dependent: :destroy
   enum payment_method: { card: 0, transfer: 1 }
+  enum status: { waiting: 0, confirmation: 1, producting: 2, ready: 3, complete: 4 }
 end
