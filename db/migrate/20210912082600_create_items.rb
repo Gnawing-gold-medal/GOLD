d282bebe5894b76429bee9a9c08210cd0782a720
304089 <9457imai2@gmail.com>
2021-09-15 02:41:30 +0000

    itemマイグレーション修正

--- db/migrate/20210912082600_create_items.rb
+++ db/migrate/20210912082600_create_items.rb
@@ -2,11 +2,11 @@ class CreateItems < ActiveRecord::Migration[5.2]
   def change
     create_table :items do |t|
       t.integer :genre_id, null: false
-      t.string :item_name, null: false
-      t.text :explanation, null: false
-      t.float :price, null: false
-      t.string :sale_status, null: false
-      t.string :item_image_id, null: false
+      t.string :name, null: false
+      t.text :introduction, null: false
+      t.integer :price, null: false
+      t.boolean :is_active, null: false, default: true
+      t.string :image_id, null: false
 
       t.timestamps
     end
