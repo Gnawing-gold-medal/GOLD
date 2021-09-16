8b419b315ad34346648248def30bf954e7f61060
seitosugino <0jb4f61k3853v2f@gmail.com>
2021-09-16 10:53:50 +0000

    オーダーモデル変更

---
+++ app/helpers/admins/order_items_helper.rb
@@ -0,0 +1,2 @@
+module Admins::OrderItemsHelper
+end
---
+++ test/controllers/admins/order_items_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class Admins::OrderItemsControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
