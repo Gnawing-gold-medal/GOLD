6a6b0a9ffe33bc29577693ca3c4b729c47151613
304089 <9457imai2@gmail.com>
2021-09-16 21:43:37 +0000

    注文機能OK

--- app/controllers/customers/orders_controller.rb
+++ app/controllers/customers/orders_controller.rb
@@ -9,13 +9,42 @@ class Customers::OrdersController < ApplicationController
 
   def log
     @customer= Customer.find(params[:customer_id])
+    @addresses= Address.where(customer_id: @customer.id)
     @cart_items= CartItem.where(customer_id: @customer.id)
     total_price= []
     @cart_items.each do |cart_item|
       total_price << (cart_item.item.price * 1.1).round * cart_item.amount
     end
     @total_price= total_price.sum
+    @total_payment= @total_price + 800
+
+    @order= Order.new(order_params)
+    if params[:order][:address_select] == "0"
+       @order_postal_code= @customer.postal_code
+       @order_address= @customer.address
+       @order_name= @customer.last_name + @customer.first_name
+    elsif params[:order][:address_select] == "1"
+       @order_postal_code = Address.find_by(id: params[:order][:oter_address], customer_id: @customer.id).postal_code
+       @order_address = Address.find_by(id: params[:order][:oter_address], customer_id: @customer.id).address
+       @order_name = Address.find_by(id: params[:order][:oter_address], customer_id: @customer.id).name
+    elsif params[:order][:address_select] == "2"
+       @order_postal_code= @order.postal_code
+       @order_address= @order.address
+       @order_name= @order.name
+    end
+
   end
+
+  def create
+    order= Order.new(order_params)
+    order.customer_id = current_customer.id
+    order.save
+    redirect_to thanks_customers_customer_orders_path(current_customer.id)
+  end
+
+  def thanks
+  end
+
 
   def index
     @orders= Order.where(customer_id: params[:customer_id])
@@ -24,7 +53,7 @@ class Customers::OrdersController < ApplicationController
   end
 
   def show
-    @order= Order.find_by(id: params[:id],customer_id: params[:customer_id])
+    @order= Order.find_by(customer_id: params[:customer_id])
     @order_items= OrderItem.where(order_id: @order.id)
     total_price= []
     @order_items.each do |order_item|
@@ -33,9 +62,8 @@ class Customers::OrdersController < ApplicationController
     @total_price= total_price.sum
   end
 
-
-
   private
+
   def order_params
     params.require(:order).permit(:postal_code, :address, :name, :shipping_cost, :total_payment, :payment_method, :status)
   end
--- app/models/customer.rb
+++ app/models/customer.rb
@@ -8,4 +8,8 @@ class Customer < ApplicationRecord
   has_many :cart_items, dependent: :destroy
   has_many :orders, dependent: :destroy
 
+  def full_name
+    self.last_name + self.first_name
+  end
+
 end
