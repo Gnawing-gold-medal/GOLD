7bc9401871008f77d22c0804f794683f7d82b393
seitosugino <0jb4f61k3853v2f@gmail.com>
2021-09-16 07:01:55 +0000

    オーダモデル

--- app/controllers/admins/orders_controller.rb
+++ app/controllers/admins/orders_controller.rb
@@ -8,9 +8,14 @@ class Admins::OrdersController < ApplicationController
     @order_items = @order.order_items
   end
   
+  
+  
   private
     def order_params
       params.require(:order).permit(:customer_id, :postal_code, :address, :name, :shipping_cost, :total_payment, :payment_method, :status)
+    end
+    def order_item_params
+      params.require(:order_item).permit(:order_id, :item_id, :price, :amount, :making_status)
     end
 
 
