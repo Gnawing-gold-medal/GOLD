351a7ae96ccee0250738c8d59b967e5b84bde183
usskr <rei.takagi731@gmail.com>
2021-09-16 04:59:31 +0000

    カートの追加/一覧/一部削除機能追加

--- app/controllers/customers/cart_items_controller.rb
+++ app/controllers/customers/cart_items_controller.rb
@@ -1,2 +1,37 @@
 class Customers::CartItemsController < ApplicationController
+
+  def create
+    @cart_item = CartItem.new(cart_item_params)
+    @cart_item.customer_id = current_customer.id
+    if @cart_item.save
+      redirect_to customers_cart_items_path(@cart_item)
+    end
+  end
+
+  def index
+    @cart_items = current_customer.cart_items.all
+    @total = @cart_items.inject(0) { |sum, item| sum + item.sum_of_price }
+  end
+
+  def update
+
+  end
+
+  def destroy
+    cart_item = CartItem.find(params[:id])
+    if cart_item.destroy
+      redirect_to customers_cart_items_path(@cart_item)
+    end
+  end
+
+  def cart_destroy
+
+  end
+
+  private
+
+  def cart_item_params
+    params.require(:cart_item).permit(:amount, :item_id)
+  end
+
 end
--- app/models/cart_item.rb
+++ app/models/cart_item.rb
@@ -1,4 +1,9 @@
 class CartItem < ApplicationRecord
-  belongs_to :customaer
+  belongs_to :customer
   belongs_to :item
+  
+  def sum_of_price
+    item.taxin_price * amount
+  end 
+  
 end
