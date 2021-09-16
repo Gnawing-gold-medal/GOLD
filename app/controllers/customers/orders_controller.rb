99552051e83152a51aca102d927580150ceec8f1
304089 <9457imai2@gmail.com>
2021-09-16 11:13:43 +0000

    注文機能更新

--- app/controllers/customers/orders_controller.rb
+++ app/controllers/customers/orders_controller.rb
@@ -1,13 +1,20 @@
 class Customers::OrdersController < ApplicationController
 
   def new
-    @customer = Customer.find(params[:customer_id])
-    @addresses = Address.where(customer_id: @customer.id)
+    @customer= Customer.find(params[:customer_id])
+    @address= Address.find_by(customer_id: params[:customer_id])
+    @addresses= Address.where(customer_id: @customer.id)
     @order= Order.new
   end
 
   def log
-    @order= Order.find_by(customer_id: params[:customer_id])
+    @customer= Customer.find(params[:customer_id])
+    @cart_items= CartItem.where(customer_id: @customer.id)
+    total_price= []
+    @cart_items.each do |cart_item|
+      total_price << (cart_item.item.price * 1.1).round * cart_item.amount
+    end
+    @total_price= total_price.sum
   end
 
   def index
@@ -19,7 +26,7 @@ class Customers::OrdersController < ApplicationController
   def show
     @order= Order.find_by(id: params[:id],customer_id: params[:customer_id])
     @order_items= OrderItem.where(order_id: @order.id)
-    total_price = []
+    total_price= []
     @order_items.each do |order_item|
       total_price << order_item.price * order_item.amount
     end
@@ -29,7 +36,7 @@ class Customers::OrdersController < ApplicationController
 
 
   private
-  def prder_params
+  def order_params
     params.require(:order).permit(:postal_code, :address, :name, :shipping_cost, :total_payment, :payment_method, :status)
   end
 
