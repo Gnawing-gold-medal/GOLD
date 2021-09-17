1b619faef477363137b8c1d0f50079b87d0bb504
304089 <9457imai2@gmail.com>
2021-09-17 06:23:01 +0000

    注文機能修正

--- app/models/cart_item.rb
+++ app/models/cart_item.rb
@@ -1,9 +1,14 @@
 class CartItem < ApplicationRecord
   belongs_to :customer
   belongs_to :item
-  
+
   def sum_of_price
     item.taxin_price * amount
-  end 
-  
+  end
+
+  def taxin_price
+    (price*1.08).round
+  end
+
+
 end
