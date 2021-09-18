930dbc86e9b733296d5e97acdcc215b4e20e7699
304089 <9457imai2@gmail.com>
2021-09-18 06:56:24 +0000

    マイページレイアウト作成

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -7,11 +7,9 @@ class Customer < ApplicationRecord
   has_many :addresses, dependent: :destroy
   has_many :cart_items, dependent: :destroy
   has_many :orders, dependent: :destroy
-  
-  enum is_deleted: { 有効: false, 退会: true }
 
   def full_name
     self.last_name + self.first_name
   end
-  
+
 end
