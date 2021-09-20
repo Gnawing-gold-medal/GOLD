db18e5fbb01a4c1e658216df2bb560d3f28d3e49
304089 <9457imai2@gmail.com>
2021-09-20 11:02:20 +0000

    ヘッダー修正

--- app/models/order.rb
+++ app/models/order.rb
@@ -2,10 +2,10 @@ class Order < ApplicationRecord
   belongs_to :customer
   has_many :order_items, dependent: :destroy
 
-  validates :postal_code, {presence: true}
+  validates :postal_code, {presence: true ,numericality: true, length: { is: 7 } }
   validates :address, {presence: true}
   validates :name, {presence: true}
-  
+
 
   enum payment_method: { "クレジットカード": 0, "銀行振込": 1 }
   enum status: { "入金待ち": 0, "入金確認": 1, "製作中": 2, "発送準備中": 3, "発送済み": 4 }
