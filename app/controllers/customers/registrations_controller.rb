594c86beca060b7ae35642eeb54260057677fc6f
usskr <rei.takagi731@gmail.com>
2021-09-21 05:52:23 +0000

    遷移先修正

--- app/controllers/customers/registrations_controller.rb
+++ app/controllers/customers/registrations_controller.rb
@@ -51,12 +51,13 @@ class Customers::RegistrationsController < Devise::RegistrationsController
   # end
 
   # The path used after sign up.
-  # def after_sign_up_path_for(resource)
-  #   super(resource)
-  # end
+  def after_sign_up_path_for(resource)
+    customers_customer_path(resource)
+  end
 
   # The path used after sign up for inactive accounts.
-  # def after_inactive_sign_up_path_for(resource)
-  #   super(resource)
-  # end
+  def after_inactive_sign_up_path_for(resource)
+    customers_customer_path(resource)
+  end
+
 end
