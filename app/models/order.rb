e0c1c42e4838fd681acbefd705b9946f2d3c2342
seitosugino <0jb4f61k3853v2f@gmail.com>
2021-09-21 12:43:49 +0000

    ステータス連動その２

--- app/models/order.rb
+++ app/models/order.rb
@@ -10,6 +10,13 @@ class Order < ApplicationRecord
   enum payment_method: { "クレジットカード": 0, "銀行振込": 1 }
   enum status: { "入金待ち": 0, "入金確認": 1, "製作中": 2, "発送準備中": 3, "発送済み": 4 }
   
+  def self.multi_update(order_params)
+    order_params.to_h.map do |id, order_param|
+      order = self.find(id)
+      order.update_attributes!(order_param)
+    end
+  end
+  
 
   def self.search(method)
     if method == '入金待ち'
