c6bc180eac09bca59f5132d254cc790403cd6f56
seitosugino <0jb4f61k3853v2f@gmail.com>
2021-09-21 13:23:03 +0000

    ルーティング修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 Rails.application.routes.draw do
-  get 'search/search'
+
   devise_for :admins, controllers: {
    sessions:      'admins/sessions',
     passwords:     'admins/passwords',
    registrations: 'admins/registrations'
   }
-
+  
   # adminのログアウト時にルーティングエラーになったため追加
   devise_scope :admin do
     get '/admins/sign_out' => 'admins/sessions#destroy'
@@ -15,51 +15,55 @@ Rails.application.routes.draw do
     sessions:      'customers/sessions',
     passwords:     'customers/passwords',
     registrations: 'customers/registrations'
-}
+   }
 
   root :to => "customers/items#top"
   get "about" => "customers/items#about"
-
+  
   resources :infomations do
      collection do
        get :thanks
      end
   end
 
-  resources :order_items do
-    resources :items
-  end
-
-
   namespace :admins do
-    resources :genres
-    resources :items
-    resources :orders
-    resources :customers
+    resources :customers,only: [:index,:show,:edit,:update]
+  	resources :items,only: [:index,:new,:create,:show,:edit,:update,]
+  	resources :genres,only: [:index,:create,:edit,:update, :show]
+  	resources :orders,only: [:index,:show,:update]
+    
   end
+
+
 
   namespace :customers do
-    resources :items
+    resources :items,only: [:index,:show]
     resources :cart_items do
       collection do
         delete :cart_destroy
       end
     end
-    resources :customers do
-      member do
+
+  	resource :customers,only: [:show] do
+  		member do
         get :leave_page
         patch :leave
       end
-      resources :addresses
-      resources :orders do
+
+      resources :cart_items,only: [:index,:update,:create,:destroy] do
+        collection do
+          delete :cart_destroy
+        end
+      end
+
+      resources :orders,only: [:new,:index,:show,:create] do
         collection do
           post :log
           get :thanks
         end
       end
+
+      resources :addresses,only: [:index,:create,:edit,:update,:destroy]
     end
   end
-
-
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
