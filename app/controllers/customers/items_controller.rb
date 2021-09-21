310c4b2766f7b89930e241268cb0104285295d9c
seitosugino <0jb4f61k3853v2f@gmail.com>
2021-09-21 02:29:38 +0000

    トップにランキング

--- app/controllers/customers/items_controller.rb
+++ app/controllers/customers/items_controller.rb
@@ -2,9 +2,10 @@ class Customers::ItemsController < ApplicationController
 
   def top
     @items = Item.all
+    @all_ranks = Item.find(OrderItem.group(:item_id).order('count(item_id) desc').limit(4).pluck(:item_id))
   end
 
-  def about
+  def aboutt
   end
 
   def index
