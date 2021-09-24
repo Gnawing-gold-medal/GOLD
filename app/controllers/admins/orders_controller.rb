9d13d69f3acf326d5bf9b834245b2d71b5d8b0d3
usskr <rei.takagi731@gmail.com>
2021-09-24 01:05:32 +0000

    注文者詳細へのリンクを追加

--- app/controllers/admins/orders_controller.rb
+++ app/controllers/admins/orders_controller.rb
@@ -1,12 +1,12 @@
 class Admins::OrdersController < ApplicationController
   def index
     @orders = Order.search(params[:method]).page(params[:page]).per(20)
-    @data = [[Date.today.ago(6.days), @orders.created_6day_ago.count], 
-    [Date.today.ago(5.days), @orders.created_5day_ago.count], 
-    [Date.today.ago(4.days), @orders.created_4day_ago.count], 
-    [Date.today.ago(3.days), @orders.created_3day_ago.count], 
-    [Date.today.ago(2.days), @orders.created_2day_ago.count], 
-    [Date.yesterday, @orders.created_yesterday.count], 
+    @data = [[Date.today.ago(6.days), @orders.created_6day_ago.count],
+    [Date.today.ago(5.days), @orders.created_5day_ago.count],
+    [Date.today.ago(4.days), @orders.created_4day_ago.count],
+    [Date.today.ago(3.days), @orders.created_3day_ago.count],
+    [Date.today.ago(2.days), @orders.created_2day_ago.count],
+    [Date.yesterday, @orders.created_yesterday.count],
     [Date.today, @orders.created_today.count]]
   end
 
@@ -26,7 +26,7 @@ class Admins::OrdersController < ApplicationController
       else
         redirect_to admins_order_path(@order), notice: "You have updated status successfully."
       end
-      
+
   end
 
   private
@@ -34,7 +34,7 @@ class Admins::OrdersController < ApplicationController
       params.require(:order).permit(:customer_id, :postal_code, :address, :name, :shipping_cost, :total_payment, :payment_method, :status)
     end
     private
-    
+
     def order_item_params
       params.require(:order).permit(order_items: :making_status)[:order_items]
     end
